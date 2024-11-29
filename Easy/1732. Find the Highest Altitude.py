b6737cedab3e5c20c236e67a1a1bc93f21994df6
Rushil Mahadevu <103291736+RushilMahadevu@users.noreply.github.com>
2024-11-29 08:29:59 -0800

    Create 1732. Find the Highest Altitude.py

---
+++ Easy/1732. Find the Highest Altitude.py
@@ -0,0 +1,9 @@
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        curSum = 0
+        maxSum = 0
+
+        for i in range(len(gain)):
+            curSum += gain[i]
+            maxSum = max(maxSum, curSum)
+        return maxSum
